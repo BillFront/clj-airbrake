3df52273e2c38128ccc1b671e8f363c3864c7886
Johannes Barre <igel@igels.net>
2016-07-26 11:58:51 +0200

    Private settings

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-airbrake "3.0.4"
+(defproject clj-airbrake "3.0.4.billfront"
   :description "Airbrake Client"
   :min-lein-version "2.0.0"
   :url "https://github.com/bmabey/clj-airbrake"
@@ -9,4 +9,14 @@
                  [cheshire "5.5.0"]]
   :profiles {:dev
              {:resource-paths ["test-resources"]
-              :dependencies [[enlive "1.1.4"]]}})
+              :dependencies [[enlive "1.1.4"]]}}
+  :plugins [[s3-wagon-private "1.2.0"]]
+  :repositories ~(let [split #(clojure.string/split %2 %1)
+                       [username passphrase]
+                       (->> (str (System/getenv "HOME") "/.aws/credentials") slurp
+                            (split #"\n") rest
+                            (map (comp last (partial split #" = "))))]
+                   {"private" {:url "s3p://billfront-sftp/public/"
+                               :sign-releases false
+                               :username username
+                               :passphrase passphrase}}))
